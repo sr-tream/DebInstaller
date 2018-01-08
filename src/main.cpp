33c101873f22d32ee36bbbbebe1d5fc86491f6ba
SR_team <sr-tream@yandex.ru>
2018-01-08 19:48:46 +0300

    Initial import

---
+++ src/main.cpp
@@ -0,0 +1,64 @@
+/*
+ Copyright (C) %{CURRENT_YEAR} by %{AUTHOR} <%{EMAIL}>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation; either version 2 of
+ the License or (at your option) version 3 or any later version
+ accepted by the membership of KDE e.V. (or its successor approved
+ by the membership of KDE e.V.), which shall act as a proxy 
+ defined in Section 14 of version 3 of the license.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// application header
+#include "debinstallerwindow.h"
+
+// KF headers
+#include <KAboutData>
+#include <KLocalizedString>
+
+// Qt headers
+#include <QApplication>
+#include <QCommandLineParser>
+
+int main(int argc, char **argv)
+{
+    QApplication application(argc, argv);
+
+    KLocalizedString::setApplicationDomain("debinstaller");
+
+    KAboutData aboutData( QStringLiteral("debinstaller"),
+                          i18n("Simple App"),
+                          QStringLiteral("%{VERSION}"),
+                          i18n("A Simple Application written with KDE Frameworks"),
+                          KAboutLicense::GPL,
+                          i18n("Copyright %{CURRENT_YEAR}, %{AUTHOR} <%{EMAIL}>"));
+
+    aboutData.addAuthor(i18n("%{AUTHOR}"),i18n("Author"), QStringLiteral("%{EMAIL}"));
+    aboutData.setOrganizationDomain("example.org");
+    aboutData.setDesktopFileName(QStringLiteral("org.example.debinstaller"));
+
+    KAboutData::setApplicationData(aboutData);
+    application.setWindowIcon(QIcon::fromTheme(QStringLiteral("debinstaller")));
+
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+    aboutData.setupCommandLine(&parser);
+
+    parser.process(application);
+    aboutData.processCommandLine(&parser);
+
+    DebInstallerWindow *mainWindow = new DebInstallerWindow;
+    mainWindow->show();
+
+    return application.exec();
+}
